953f54d93707d24c4d50fef3584aa137ca69e8e2
Alexander Alkhimyonok <buckartz@gmail.com>
2018-03-27 14:32:00 +0300

    Project configuration

---
+++ src/main/resources/db/config/1_db_user_creation.sql
@@ -0,0 +1,13 @@
+# Create user with name root and password root through pgAdmin if it doesn't exist
+# This query is from there
+
+# CREATE USER root
+# WITH
+# LOGIN
+# SUPERUSER
+# CREATEDB
+# CREATEROLE
+# INHERIT
+# REPLICATION
+# CONNECTION LIMIT -1
+# PASSWORD 'xxxxxx';
---
+++ src/main/resources/db/config/2_database_creation.sql
@@ -0,0 +1,5 @@
+# Create database coffee_shop through pgAdmin
+
+# Also you should have h2 database on your machine
+
+# You ready to use app
